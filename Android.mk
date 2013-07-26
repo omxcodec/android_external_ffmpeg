e3f95ecd278a940a57a09df05dd333445bd050b7
Michael Chen <omxcodec@gmail.com>
2013-07-27 00:51:18 +0800

    fix for cm10.1, based on ffmpeg-1.0

--- Android.mk
+++ Android.mk
@@ -2,7 +2,8 @@ include $(CLEAR_VARS)
 FFMPEG_TCDIR := $(realpath $(shell dirname $(TARGET_TOOLS_PREFIX)))
 FFMPEG_TCPREFIX := $(shell basename $(TARGET_TOOLS_PREFIX))
 # FIXME remove -fno-strict-aliasing once the aliasing violations are fixed
-FFMPEG_COMPILER_FLAGS = $(subst -I ,-I../../,$(subst -include system/core/include/arch/linux-arm/AndroidConfig.h,,$(TARGET_GLOBAL_CFLAGS))) -fno-strict-aliasing -Wno-error=address -Wno-error=format-security
+FFMPEG_COMPILER_FLAGS = $(subst -I ,-I../../,$(subst -include system/core/include/arch/linux-arm/AndroidConfig.h,,$(subst -include build/core/combo/include/arch/linux-arm/AndroidConfig.h,,$(TARGET_GLOBAL_CFLAGS)))) -fno-strict-aliasing -Wno-error=address -Wno-error=format-security
+
 ifneq ($(strip $(SHOW_COMMANDS)),)
 FF_VERBOSE="V=1"
 endif
@@ -33,8 +34,8 @@ ffmpeg: $(PRODUCT_OUT)/obj/STATIC_LIBRARIES/libvpx_intermediates/libvpx.a
 		--disable-libvpx \
 		--enable-cross-compile \
 		--cross-prefix=$(FFMPEG_TCPREFIX) \
-		--extra-ldflags="-nostdlib -Wl,-dynamic-linker,/system/bin/linker,-z,muldefs$(shell if test $(PRODUCT_SDK_VERSION) -lt 16; then echo -n ',-T$(REALTOP)/$(BUILD_SYSTEM)/armelf.x'; fi),-z,nocopyreloc,--no-undefined -L$(REALTOP)/$(TARGET_OUT_STATIC_LIBRARIES) -L$(REALTOP)/$(PRODUCT_OUT)/system/lib -L$(REALTOP)/$(PRODUCT_OUT)/obj/STATIC_LIBRARIES/libvpx_intermediates -ldl -lc" \
+		--extra-ldflags="-nostdlib -Wl,-dynamic-linker,/system/bin/linker,-z,muldefs$(shell if test $(PRODUCT_SDK_VERSION) -lt 16; then echo -n ',-T$(REALTOP)/$(BUILD_SYSTEM)/armelf.x'; fi),-z,nocopyreloc,--no-undefined -L$(PRODUCT_OUT)/obj/STATIC_LIBRARIES -L$(PRODUCT_OUT)/system/lib -L$(PRODUCT_OUT)/obj/STATIC_LIBRARIES/libvpx_intermediates -ldl -lc" \
 		--extra-cflags="$(FFMPEG_COMPILER_FLAGS) -I$(REALTOP)/bionic/libc/include -I$(REALTOP)/bionic/libc/kernel/common -I$(REALTOP)/bionic/libc/kernel/arch-arm -I$(REALTOP)/bionic/libc/arch-arm/include -I$(REALTOP)/bionic/libm/include -I$(REALTOP)/external/libvpx" \
 		--extra-libs="-lgcc" && \
-	$(MAKE) TARGET_CRTBEGIN_DYNAMIC_O=$(REALTOP)/$(TARGET_CRTBEGIN_DYNAMIC_O) TARGET_CRTEND_O=$(REALTOP)/$(TARGET_CRTEND_O) $(FF_VERBOSE) && \
-	$(MAKE) install-libs DESTDIR=$(REALTOP)/$(PRODUCT_OUT)
+	$(MAKE) TARGET_CRTBEGIN_DYNAMIC_O=$(TARGET_CRTBEGIN_DYNAMIC_O) TARGET_CRTEND_O=$(TARGET_CRTEND_O) $(FF_VERBOSE) && \
+	$(MAKE) install-libs DESTDIR=$(PRODUCT_OUT)
